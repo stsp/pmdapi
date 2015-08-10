ba963e4461687b2a48c148fe5f1131eb0cd024a2
Stas Sergeev <stsp@users.sourceforge.net>
2015-08-10 04:46:07 +0300

    use __djgpp_ds_alias as dseg32

--- src/entry.h
+++ src/entry.h
@@ -1,6 +1,13 @@
 #ifndef __ENTRY_H
 #define __ENTRY_H
 
+#define _dseg32 ___djgpp_ds_alias
+
+#ifndef __ASSEMBLER__
+
+#include <sys/exceptn.h>
+
+#define dseg32 __djgpp_ds_alias
 extern void entry32(void);
 extern void dos32_int21(void);
 extern void dos16_int21(void);
@@ -10,8 +17,8 @@ extern void code16_end(void);
 extern void data16(void);
 extern unsigned long cs32_desc[2];
 extern unsigned long ds32_desc[2];
-extern unsigned long dseg32;
 extern unsigned long is_32;
 extern void data16_end(void);
 
+#endif
 #endif
