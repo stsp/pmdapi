a2c46656331741da4a1cba745c40740bbd7e0c95
Stas Sergeev <stsp@users.sourceforge.net>
2021-01-08 00:58:29 +0300

    enable THUNK_16_32/THUNK_16_32x extensions

--- src/startup.c
+++ src/startup.c
@@ -13,11 +13,11 @@ unsigned char fs_desc[8], gs_desc[8];
 
 int main()
 {
-  __dpmi_callback_info info;
+  __dpmi_callback_info info = {};
+  __dpmi_paddr extapi;
   unsigned long desc[2];
   short cs16, cs32, ds16, ds32;
   unsigned long cs_base, ds_base;
-  modify_ldt_t ldt;
 
   emm_init();
   wrapper_init();
@@ -34,39 +34,63 @@ int main()
   }
   printf("cs=%#hx ds=%#hx cs_base=%#lx ds_base=%#lx\n", cs32, ds32, cs_base, ds_base);
 
-  ldt.seg_32bit = 0;
-  ldt.read_exec_only = 0;
-  ldt.limit_in_pages = 0;
-  ldt.seg_not_present = 0;
+  if (__dpmi_get_vendor_specific_api_entry_point("THUNK_16_32", &extapi)) {
+    printf("THUNK_16_32 not supported\n");
+    cs16 = 0;
+  } else {
+    modify_ldt_t ldt;
 
-  if ((cs16 = __dpmi_allocate_ldt_descriptors(1)) == -1) {
-    printf("alloc_desc failed\n");
-    return 1;
-  }
-  ldt.entry_number = cs16 >> 3;
-  ldt.base_addr = cs_base + (unsigned long)code16;
-  ldt.limit = (unsigned long)code16_end - (unsigned long)code16;
-  ldt.contents = MODIFY_LDT_CONTENTS_CODE;
-  desc[0] = LDT_entry_a(&ldt);
-  desc[1] = LDT_entry_b(&ldt);
-  if (__dpmi_set_descriptor(cs16, desc) == -1) {
-    printf("set_desc failed\n");
-    return 1;
-  }
+    ldt.seg_32bit = 0;
+    ldt.read_exec_only = 0;
+    ldt.limit_in_pages = 0;
+    ldt.seg_not_present = 0;
 
-  if ((ds16 = __dpmi_allocate_ldt_descriptors(1)) == -1) {
-    printf("alloc_desc failed\n");
-    return 1;
+    if ((cs16 = __dpmi_allocate_ldt_descriptors(1)) == -1) {
+      printf("alloc_desc failed\n");
+      return 1;
+    }
+    ldt.entry_number = cs16 >> 3;
+    ldt.base_addr = cs_base + (unsigned long)code16;
+    ldt.limit = (unsigned long)code16_end - (unsigned long)code16;
+    ldt.contents = MODIFY_LDT_CONTENTS_CODE;
+    desc[0] = LDT_entry_a(&ldt);
+    desc[1] = LDT_entry_b(&ldt);
+    if (__dpmi_set_descriptor(cs16, desc) == -1) {
+      printf("set_desc failed\n");
+      return 1;
+    }
+    memcpy(info.code16, desc, sizeof(desc));
+
+    if ((ds16 = __dpmi_allocate_ldt_descriptors(1)) == -1) {
+      printf("alloc_desc failed\n");
+      return 1;
+    }
+    ldt.entry_number = ds16 >> 3;
+    ldt.base_addr = ds_base + (unsigned long)data16;
+    ldt.limit = (unsigned long)data16_end - (unsigned long)data16;
+    ldt.contents = MODIFY_LDT_CONTENTS_DATA;
+    desc[0] = LDT_entry_a(&ldt);
+    desc[1] = LDT_entry_b(&ldt);
+    if (__dpmi_set_descriptor(ds16, desc) == -1) {
+      printf("set_desc failed\n");
+      return 1;
+    }
+    memcpy(info.data16, desc, sizeof(desc));
+
+    info.ip = (unsigned long)entry16 - (unsigned long)code16;
   }
-  ldt.entry_number = ds16 >> 3;
-  ldt.base_addr = ds_base + (unsigned long)data16;
-  ldt.limit = (unsigned long)data16_end - (unsigned long)data16;
-  ldt.contents = MODIFY_LDT_CONTENTS_DATA;
-  desc[0] = LDT_entry_a(&ldt);
-  desc[1] = LDT_entry_b(&ldt);
-  if (__dpmi_set_descriptor(ds16, desc) == -1) {
-    printf("set_desc failed\n");
-    return 1;
+  if (cs16) {
+    char nsup = 0;
+    /* enabling the extension is a bit tricky */
+    asm volatile("lcalll *%0\n" :: "m"(extapi), "a"(1));
+    asm volatile(
+      "int $0x2f\n"
+      "setcb %0\n"
+      : "=r"(nsup): "a"(0x168a), "S"("THUNK_16_32x"));
+    if (nsup) {
+      printf("THUNK_16_32x not supported\n");
+      return 1;
+    }
   }
 
   if (__dpmi_get_descriptor(cs32, cs32_desc) == -1) {
@@ -91,15 +115,7 @@ int main()
     }
   }
 
-  if (__dpmi_get_descriptor(cs16, info.code16) == -1) {
-    printf("get_desc failed\n");
-    return 1;
-  }
   if (__dpmi_get_descriptor(cs32, info.code32) == -1) {
-    printf("get_desc failed\n");
-    return 1;
-  }
-  if (__dpmi_get_descriptor(ds16, info.data16) == -1) {
     printf("get_desc failed\n");
     return 1;
   }
@@ -107,7 +123,6 @@ int main()
     printf("get_desc failed\n");
     return 1;
   }
-  info.ip = (unsigned long)entry16 - (unsigned long)code16;
   info.eip = (unsigned long)entry32;
 
   if (__dpmi_install_resident_service_provider_callback(&info) == -1) {
